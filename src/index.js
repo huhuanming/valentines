dcc20673510dbb97eb89879138f0009cf8017b34
huhuanming <huhuanming@meituan.com>
2020-02-13 15:46:22 +0800

    支持 svg

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import Pressure from 'pressure'
 import './index.css'
 import registerServiceWorker from './registerServiceWorker'
 
-
 registerServiceWorker()
 
 const valentines = document.getElementById('valentines')
@@ -10,6 +9,23 @@ const heart = document.getElementById('heart')
 const circle = document.getElementById('circle')
 
 let ended = false
+
+const createSvg = (path) => {
+  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+  svg.setAttribute('width', '2048')
+  svg.setAttribute('height', '2048')
+  svg.setAttribute('viewBox', '0 0 2048 2048')
+  svg.appendChild(path)
+  return svg
+}
+const createSvgPath = (d) => {
+  const path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+  path.setAttribute('d', d)
+  return path
+}
+
+const heartSvg = createSvg(createSvgPath('M1024 1792q-26 0-44-18l-624-602q-10-8-27.5-26t-55.5-65.5-68-97.5-53.5-121-23.5-138q0-220 127-344t351-124q62 0 126.5 21.5t120 58 95.5 68.5 76 68q36-36 76-68t95.5-68.5 120-58 126.5-21.5q224 0 351 124t127 344q0 221-229 450l-623 600q-18 18-44 18z'))
+const heartOSvg = createSvg(createSvgPath('M1792 724q0-81-21.5-143t-55-98.5-81.5-59.5-94-31-98-8-112 25.5-110.5 64-86.5 72-60 61.5q-18 22-49 22t-49-22q-24-28-60-61.5t-86.5-72-110.5-64-112-25.5-98 8-94 31-81.5 59.5-55 98.5-21.5 143q0 168 187 355l581 560 580-559q188-188 188-356zm128 0q0 221-229 450l-623 600q-18 18-44 18t-44-18l-624-602q-10-8-27.5-26t-55.5-65.5-68-97.5-53.5-121-23.5-138q0-220 127-344t351-124q62 0 126.5 21.5t120 58 95.5 68.5 76 68q36-36 76-68t95.5-68.5 120-58 126.5-21.5q224 0 351 124t127 344z'))
 
 const start = () => {
   document.body.classList.add('start')
@@ -25,29 +41,14 @@ const reset = () => {
   document.body.classList.remove('start', 'end')
 }
 
-// Load icon svgs inline so they can be styled and not a font.
-const iconsPath = 'lib/Font-Awesome-SVG-PNG/black/svg/'
-const loadFontIcon = (name) => {
-  const icon = `${iconsPath + name}.svg`
-  // const id = `fa-${name}`
-
-  const xhr = new XMLHttpRequest()
-  xhr.open('GET', icon, true)
-  xhr.onload = () => {
-    // const loveIcon = xhr.responseXML.documentElement
-    // loveIcon.id = id
-    // loveIcon.classList.add(id)
-    // element.appendChild(icon)
-  }
-
-  // Following line is just to be on the safe side;
-  // not needed if your server delivers SVG with correct MIME type
-  xhr.overrideMimeType('image/svg+xml')
-  xhr.send('')
+const loadFontIcon = (id, svg, element) => {
+  svg.id = id
+  svg.classList.add(id)
+  element.appendChild(svg)
 }
 
-loadFontIcon('heart', heart)
-loadFontIcon('heart-o', heart)
+loadFontIcon('fa-heart', heartSvg, heart)
+loadFontIcon('fa-heart-o', heartOSvg, heart)
 
 // Without force touch.
 let timeout
